d529317e297335da49e295370374e64a306095f7
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-10-11 14:17:01 +0300

    Initial commit

---
+++ src/Timmi_problem.java
@@ -0,0 +1,19 @@
+public class Timmi_problem {
+
+    function lovefunc(flower1,flower2){
+        if (flower1%2==0&&flower2%2!==0) {
+            return true;
+        }
+        else if(flower1%2!==0&&flower2%2==0)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+
+
+
+
+
+}
